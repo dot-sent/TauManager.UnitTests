b45a8a3f241f4843f80af6a4b85cb93d8903e164
Dotsent <sergey.kudriavtsev@gmail.com>
2020-06-14 23:58:18 +0200

    Added more exclusions to report generation script

--- repgen.sh
+++ repgen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dotnet build && dotnet test --no-build -v normal /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov && reportgenerator -reports:TestResults/coverage.info -targetdir:reports
+dotnet build && dotnet test --no-build -v normal /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude=[TauManager.Views]* /p:exclude-by-attribute=CompilerGeneratedAttribute /p:ExcludeByFile=\"../TauManager/Areas/Identity/*.cs\" && reportgenerator -reports:TestResults/coverage.info -targetdir:reports
 
